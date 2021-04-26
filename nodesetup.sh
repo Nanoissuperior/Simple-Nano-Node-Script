c563ea2fffe6caba255fbbb388f827a9dfbd98ee
Nanoissuperior <50946350+Nanoissuperior@users.noreply.github.com>
2021-04-26 13:58:36 +0100

    Update nodesetup.sh

--- nodesetup.sh
+++ nodesetup.sh
@@ -44,11 +44,12 @@ echo Time to install docker
 sudo apt-get remove docker docker-engine docker.io containerd runc -qq
 echo Just need to run another update
 sudo apt-get update -y -qq
-sudo apt-get install apt-transport-https ca-certificates curl gnupg-agent software-properties-common -y -qq
+sudo apt-get install apt-transport-https ca-certificates curl gnupg-agent software-properties-common -y 
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add - 
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" -y -qq
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" -y 
 sudo apt-get update -qq
 sudo apt-get install docker-ce docker-ce-cli containerd.io -y -qq
+clear
 echo "
  +-+-+-+-+-+-+ +-+-+ +-+-+-+-+-+-+-+-+-+ +-+-+
  |D|o|c|k|e|r| |i|s| |i|n|s|t|a|l|l|e|d| |:|)|
